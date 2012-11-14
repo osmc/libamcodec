6e61b67ae6697ad53d39f8180a8cd18abb835e52
davilla <davilla@4pi.com>
2012-11-14 08:41:59 -0500

    [aml] someone needs to learn the difference between local and system includes

--- amplayer/player/player_thumbnail.c
+++ amplayer/player/player_thumbnail.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <player_priv.h>
 #include <log_print.h>
+#include "player_priv.h"
 #include "thumbnail_type.h"
 
 static inline void calc_aspect_ratio(rational *ratio, struct stream *stream)
